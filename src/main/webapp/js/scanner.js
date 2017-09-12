96d0d6337c0ffadabd2a33735b18deb78d761a8b
tiago.albuquerque <tiagoamp@>
2017-09-12 16:07:49 -0300

    Scanner page layout improved.

--- src/main/webapp/js/scanner.js
+++ src/main/webapp/js/scanner.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
+
+	 // $("#div-scan-result").hide();
 	
 	$("#button-scan").on('click', function (e) {
-        e.preventDefault();
+        e.preventDefault();        
+        $("#div-scan-result").toggle();
         var from = $('#form-scan-input').find('input[name="dirpath"]').val();
         var mediatype = $('input[name="mediatype"]:checked').val(); 
         scanFiles(from, mediatype);
@@ -10,18 +13,18 @@ $(document).ready(function () {
 });
 
 function scanFiles(from, mediatype) {
-	var input = { type: mediatype, path: from };
+	var input = { type: mediatype, dirPath: from };
 	
-	console.log(input);
-	
-	$.get("http://localhost:8080/acervorama/webapi/scanner", { type: mediatype, dirPath: from }, function( data ) {
-		console.log("Data = " + data );
+	$.get("http://localhost:8080/acervorama/webapi/scanner", input, function( data ) {
+		showScanResultTable(data);
 	});
-	
-	console.log("Após get ...");
-	
+		
 }
 
-function pegaRetorno(dados) {
-	console.log(dados);
+function showScanResultTable(data) {
+	
+	console.log("Received " + data );
+	
+	$("#div-scan-result").toggle();
+	
 }
