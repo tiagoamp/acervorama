3a28273115f270d839c6e39120992b47dda10e19
tiago.albuquerque <tiagoamp@>
2017-09-15 08:58:28 -0300

    Media DB Search results table feature working.

--- src/main/webapp/js/management.js
+++ src/main/webapp/js/management.js
@@ -1,8 +1,11 @@
-var formMediaSearch = $('#form-media-search'); 
+var formMediaSearch = $('#form-media-search');
+var divSearchResult = $("#div-search-result");
+var tableResult = $("#table-search-result")
 
 $(document).ready(function () {
 	
 	$("div .ui-pnotify-container").hide();
+	divSearchResult.hide();
 	
 	$("#btn-search").on('click', function (e) {
         e.preventDefault();
@@ -13,35 +16,55 @@ $(document).ready(function () {
         var tagsParam = formMediaSearch.find('input[name="search-tags"]').val();
         var typeParam = $('input[name="mediatype"]:checked').val();
         
-        searchByParams(filepathParam, filenameParam, classificationParam, tagsParam, typeParam);
-        
-	});
-	
+        searchByParams(filepathParam, filenameParam, classificationParam, tagsParam, typeParam);        
+	});	
 	
 });
 
 function searchByParams(filepathParam, filenameParam, classificationParam, tagsParam, typeParam) {
-	
-	console.log("Path = " + filepathParam);    
-	console.log("Name = " + filenameParam);
-    console.log("Classif = " + classificationParam);
-    console.log("Tags = " + tagsParam);
-    console.log("Type = " + typeParam);
-    
-    var input = { type: typeParam, filepath: filepathParam, filename: filenameParam, classification: classificationParam, tags: tagsParam };
+	var input = { type: typeParam, filepath: filepathParam, filename: filenameParam, classification: classificationParam, tags: tagsParam };
 	
 	$.get("http://localhost:8080/acervorama/webapi/media", input, function( data ) {
-		console.log("DAta = " + data);
 		if (data.length == 0) { 
 			showInfoMessage("Search returned no results.");
 		} else {
-			//showSearchResultTable(data);
+			showSearchResultTable(data);
 		}
 	})
 	.fail( function() { showErrorMessage("Fail to search files with given parameters.") } )
 	.success( function() { showSuccessMessage("Search performed.") } );
 	
 };
+
+function showSearchResultTable(data) {
+	divSearchResult.show();	
+	var tbodyResult = tableResult.find("tbody");		
+	var itemCount = data.length;
+	
+	for (i=0; i < itemCount; i++) {
+		var item =  JSON.parse(data[i]);
+		var newRow = createNewRow(i, item.filename, item.filePath, item.type);
+		tbodyResult.append(newRow);
+	}				
+}
+
+function createNewRow(rownum, fileName, filePath, fileType) {
+	var rowTr = $("<tr>");
+	
+	var thNum = $("<th>").attr("scope","row").text(rownum+1);
+	var filenameTd = $("<td>").text(fileName);
+	var filepathTd = $("<td>").text(filePath);
+	var filetypeTd = $("<td>").text(fileType);
+	var actionTd = $("<td>").text("View");
+		
+	rowTr.append(thNum);
+	rowTr.append(filenameTd);
+	rowTr.append(filepathTd);
+	rowTr.append(filetypeTd);
+	rowTr.append(actionTd);
+	
+	return rowTr;	
+}
 
 
 function showErrorMessage(msg) {
