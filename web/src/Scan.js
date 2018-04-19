09d916e47186dc61502a8b6a3246bb9f02e60440
tiago.albuquerque <tiagoamp@>
2018-04-19 09:03:07 -0300

    Create Scanner Table component

--- web/src/Scan.js
+++ web/src/Scan.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import SideMenu from './SideMenu';
 import Header from './commom/Header';
-//import ScanForm from './scanner/ScanForm';
+import ScanTable from './scanner/ScanTable';
 
 import $ from 'jquery';
 import PubSub from 'pubsub-js';
@@ -132,46 +132,10 @@ class Scan extends Component {
                 </form>
 
                 <hr/>
+
                 
-                <h2 className="sub-header">Media Collection List</h2>
-                <hr/>
-
-                <form onSubmit={this.sendMediaToSaveForm} method="POST">
-
-                  <div className="form-group row">
-                    <div className="col-sm-10">
-                      <button type="button" className="btn btn-outline-primary">Save All</button>
-                    </div>
-                  </div>               
-
-                  <table className="table table-bordered table-hover">
-                    <thead>
-                      <tr key="header">
-                        <th scope="col" className="centered">#</th>
-                        <th scope="col">File Name</th>
-                        <th scope="col">Full Path</th>
-                        <th scope="col" className="centered">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-
-                      {
-                        this.state.mediaScannedList.map(function(item, index) {
-                              const filename = item.replace(/^.*[\\/]/, '');
-                              return (
-                                <tr key={index + 1 + item}>
-                                  <td className="centered">{index+1}</td>
-                                  <td>{filename}</td>
-                                  <td>{item}</td>
-                                  <td className="centered"><button type="button" className="btn btn-outline-info">Save</button></td>
-                                </tr>      
-                              );
-                          })
-                      }
-
-                    </tbody>
-                  </table>
-                </form>  
+                <ScanTable scannedList={this.state.mediaScannedList} />
+                
 
               </section>
 
---
+++ web/src/scanner/ScanTable.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import '../css/bootstrap.min.css';
+import '../css/pnotify.custom.min.css';
+import '../css/scanner.css';
+
+
+class ScanTable extends Component {
+ 
+  render() {
+    return (
+
+      <div>
+
+                <h2 className="sub-header">Media Collection List</h2>
+                <hr/>
+
+                <form onSubmit={this.sendMediaToSaveForm} method="POST">
+
+                  <div className="form-group row">
+                    <div className="col-sm-10">
+                      <button type="button" className="btn btn-outline-primary">Save All</button>
+                    </div>
+                  </div>               
+
+                  <table className="table table-bordered table-hover">
+                    <thead>
+                      <tr key="header">
+                        <th scope="col" className="centered">#</th>
+                        <th scope="col">File Name</th>
+                        <th scope="col">Full Path</th>
+                        <th scope="col" className="centered">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                      {
+                        this.props.scannedList.map(function(item, index) {
+                              const filename = item.replace(/^.*[\\/]/, '');
+                              return (
+                                <tr key={index + 1 + item}>
+                                  <td className="centered">{index+1}</td>
+                                  <td>{filename}</td>
+                                  <td>{item}</td>
+                                  <td className="centered"><button type="button" className="btn btn-outline-info">Save</button></td>
+                                </tr>      
+                              );
+                          })
+                      }
+
+                    </tbody>
+                  </table>
+                </form>  
+
+      </div>
+
+    );
+  }
+
+}
+
+export default ScanTable;
