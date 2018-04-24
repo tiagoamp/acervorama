1c6bb54ba60f606eca5c39183b89ec53a348fc8b
tiago.albuquerque <tiagoamp@>
2018-04-24 08:34:05 -0300

    Implement management form inputs

--- web/src/Management.js
+++ web/src/Management.js
@@ -14,9 +14,15 @@ class Management extends Component {
 
   constructor() {
     super();
-    this.state = { activePanel: 2, tags: [] };
+    this.state = { activePanel: 2, filename:'', classification:'', mediaType:'', tags: [] };
   }
 
+
+  setInputValueToState(inputName,event) {
+    let alteredInput = {};
+    alteredInput[inputName] = event.target.value; 
+    this.setState(alteredInput);
+  }
 
   handleTagsChange(tags) {
     this.setState({tags})
@@ -43,15 +49,15 @@ class Management extends Component {
 
                 <form onSubmit={this.sendSearchForm} method="POST">
                   <div className="form-group row">
-                    <label className="col-sm-2 col-form-label">File name: </label>
+                    <label className="col-sm-2 col-form-label">File Name: </label>
                     <div className="col-sm-10">
-                      <input type="text" className="form-control" id="inputFilename" name="inputFilename" value="" placeholder="Enter file name" />                                            
+                      <input type="text" className="form-control" id="inputFilename" name="inputFilename" value={this.state.filename} onChange={this.setInputValueToState.bind(this,'filename')} placeholder="Enter file name" />
                     </div>                    
                   </div>
                   <div className="form-group row">
                     <label className="col-sm-2 col-form-label">Classification: </label>
                     <div className="col-sm-10">
-                      <input type="text" className="form-control" id="inputClassification" name="inputClassification" value="" placeholder="Enter classification" />
+                      <input type="text" className="form-control" id="inputClassification" name="inputClassification" value={this.state.classification} onChange={this.setInputValueToState.bind(this,'classification')} placeholder="Enter classification" />
                     </div>                    
                   </div>
                   <div className="form-group row">
@@ -65,25 +71,25 @@ class Management extends Component {
                       <legend className="col-form-label col-sm-2 pt-0">Media Type: </legend>                      
                       <div className="col-sm-10">
                         <div className="form-check form-check-inline">
-                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputAudio" value="AUDIO" />
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputAudio" value="AUDIO" onClick={this.setInputValueToState.bind(this,'mediaType')} />
                           <label className="form-check-label" htmlFor="inputAudio">
                             Audio
                           </label>                          
                         </div>
                         <div className="form-check form-check-inline">
-                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputImage" value="IMAGE" />
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputImage" value="IMAGE" onClick={this.setInputValueToState.bind(this,'mediaType')} />
                           <label className="form-check-label" htmlFor="inputImage">
                             Image
                           </label>                          
                         </div>
                         <div className="form-check form-check-inline">
-                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputText" value="TEXT" />
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputText" value="TEXT" onClick={this.setInputValueToState.bind(this,'mediaType')} />
                           <label className="form-check-label" htmlFor="inputText">
                             Text
                           </label>                          
                         </div>
                         <div className="form-check form-check-inline">
-                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputVideo" value="VIDEO" />
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputVideo" value="VIDEO" onClick={this.setInputValueToState.bind(this,'mediaType')} />
                           <label className="form-check-label" htmlFor="inputVideo">
                             Video
                           </label>                          
