410783de1358d955bbfca62d5617b1886c052c61
tiago.albuquerque <tiagoamp@>
2018-04-24 08:22:21 -0300

    Implement management form layout

--- web/src/Management.js
+++ web/src/Management.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import TagsInput from 'react-tagsinput'
+
+import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 
 import SideMenu from './SideMenu';
 import Header from './commom/Header';
@@ -11,15 +14,21 @@ class Management extends Component {
 
   constructor() {
     super();
-    this.state = { activePanel: 2 };
+    this.state = { activePanel: 2, tags: [] };
   }
+
+
+  handleTagsChange(tags) {
+    this.setState({tags})
+  }
+
   
   render() {
     return (
 
       <div>
         
-        <Header/>
+        <Header/> 
 
         <div className="container-fluid">
           <div className="row">
@@ -28,7 +37,68 @@ class Management extends Component {
             
             <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
 
-              <h1>Test management !!!!</h1>
+              <section>
+                <h2 className="sub-header">Media Collection Management</h2>
+                <hr />
+
+                <form onSubmit={this.sendSearchForm} method="POST">
+                  <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">File name: </label>
+                    <div className="col-sm-10">
+                      <input type="text" className="form-control" id="inputFilename" name="inputFilename" value="" placeholder="Enter file name" />                                            
+                    </div>                    
+                  </div>
+                  <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Classification: </label>
+                    <div className="col-sm-10">
+                      <input type="text" className="form-control" id="inputClassification" name="inputClassification" value="" placeholder="Enter classification" />
+                    </div>                    
+                  </div>                
+                  <fieldset className="form-group">
+                    <div className="row">
+                      <legend className="col-form-label col-sm-2 pt-0">Media Type: </legend>                      
+                      <div className="col-sm-10">
+                        <div className="form-check form-check-inline">
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputAudio" value="AUDIO" />
+                          <label className="form-check-label" htmlFor="inputAudio">
+                            Audio
+                          </label>                          
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputImage" value="IMAGE" />
+                          <label className="form-check-label" htmlFor="inputImage">
+                            Image
+                          </label>                          
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputText" value="TEXT" />
+                          <label className="form-check-label" htmlFor="inputText">
+                            Text
+                          </label>                          
+                        </div>
+                        <div className="form-check form-check-inline">
+                          <input className="form-check-input" type="radio" name="inputMediaType" id="inputVideo" value="VIDEO" />
+                          <label className="form-check-label" htmlFor="inputVideo">
+                            Video
+                          </label>                          
+                        </div> 
+                      </div>
+                    </div>
+                  </fieldset>
+                  <div className="form-group row">
+                    <label className="col-sm-2 col-form-label">Tags: </label>
+                    <div className="col-sm-10">
+                      <TagsInput value={this.state.tags} onChange={this.handleTagsChange.bind(this)} />  
+                    </div>                    
+                  </div>                   
+                  <div className="form-group row">
+                    <div className="col-sm-10">
+                      <button type="submit" className="btn btn-primary">Search!</button>
+                    </div>
+                  </div>
+                </form>
+
+              </section>
 
             </main>
 
